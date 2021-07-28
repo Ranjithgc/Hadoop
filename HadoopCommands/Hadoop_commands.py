d01d5a32c7b653aa6ec574ffbdf9266ea2993682
Ranjithgc <Ranjith.amca.16@acharya.ac.in>
2021-07-28 13:26:57 +0530

    [Ranjithgc] Add: working of hadoop commands with python

---
+++ HadoopCommands/Hadoop_commands.py
@@ -0,0 +1,61 @@
+import subprocess
+
+def run_cmd(args_list):
+        """
+        run linux commands
+        """
+        print('Running system command: {0}'.format(' '.join(args_list)))
+        proc = subprocess.Popen(args_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        s_output, s_err = proc.communicate()
+        s_return =  proc.returncode
+        return s_return, s_output, s_err 
+
+if __name__ == "__main__":
+    #Run Hadoop mkdir command in python
+    mkdir = run_cmd(['hdfs', 'dfs', '-mkdir', '-p', '/data/test/ranjith'])
+    
+    #Run Hadoop ls command in Python
+    ls = run_cmd(['hdfs', 'dfs', '-ls', '/data/test'])
+
+    #Run Hadoop put command in Python
+    put = run_cmd(['hdfs', 'dfs', '-put', '/home/ubunta/Desktop/Hadoop_Python/sample.txt', '/data/test/ranjith'])
+
+    #Run Hadoop copyFromLocal command in Python
+    copyFromLocal = run_cmd(['hdfs', 'dfs', '-copyFromLocal', 'sample1.txt', '/data/test/ranjith'])
+
+    #Run Hadoop get command in Python
+    get = run_cmd(['hdfs', 'dfs', '-get', '/data/test/ranjith/sample1.txt', '/home/ubunta'])
+                      
+    #Run Hadoop copyToLocal command in Python
+    copyToLocal = run_cmd(['hdfs', 'dfs', '-copyToLocal', '/data/test/ranjith/sample.txt'])
+
+    #Run Hadoop mv command in python
+    mv = run_cmd(['hdfs', 'dfs', '-mv', '/data/test/ranjith/sample1.txt', '/user/data/abc'])
+
+    #Run Hadoop cp command in python
+    cp = run_cmd(['hdfs', 'dfs', '-cp', '/user/data/abc/sample1.txt', '/data/test/ranjith'])
+
+    #Run Hadoop moveFromLocal command in python
+    moveFromLocal = run_cmd(['hdfs', 'dfs', '-moveFromLocal', 'sample2.txt', '/data/test/ranjith'])
+
+    #Run Hadoop rm command in python
+    # rm command used to remove directories
+    rm = run_cmd(['hdfs', 'dfs', '-rm', '-r', '/test'])
+
+
+
+
+
+
+
+
+
+
+                       
+
+
+
+
+
+
+
