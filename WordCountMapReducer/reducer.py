9db9a3ff2811bf29158c9112c81885be00f996d4
Ranjithgc <Ranjith.amca.16@acharya.ac.in>
2021-07-29 12:31:04 +0530

    [Ranjithgc] Refactor: added author information

--- WordCountMapReducer/reducer.py
+++ WordCountMapReducer/reducer.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python
+"""
+@Author: Ranjith G C
+@Date: 2021-07-28
+@Last Modified by: Ranjith G C
+@Last Modified time: 2021-07-28 
+@Title : Program Aim is to work with word count by using reducer.
+"""
 
 from operator import itemgetter
 import sys
